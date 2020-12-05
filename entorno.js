c492feaae4d56ace2f80877a300f1a58f3a7c1a0
Daniel Vergara <danielvsuarez01@gmail.com>
2020-12-05 18:27:16 -0500

    env practice with nodemon

--- entorno.js
+++ entorno.js
@@ -3,3 +3,6 @@ let web = process.env.WEB || 'no tengo web';
 
 console.log('Hola ' + nombre); // NOMBRE=Daniel node entorno.js en consola
 console.log('Mi web es ' + web);
+
+console.log('otra cosa');
+console.log('otra cosa 2.0');
