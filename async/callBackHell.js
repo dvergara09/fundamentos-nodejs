79d95d1255dac13bd2c0cb0a6999ef01318e9209
Daniel Vergara <danielvsuarez01@gmail.com>
2020-12-05 19:40:10 -0500

    callbackhell

---
+++ async/callBackHell.js
@@ -0,0 +1,22 @@
+function hello(name, myCallback) {
+  setTimeout(function () {
+    console.log(`Hello ${name}`);
+    myCallback(name);
+  }, 1000);
+}
+
+function bye(name, otherCallback) {
+  setTimeout(function () {
+    console.log(`Goodbye ${name}`);
+    otherCallback();
+  });
+}
+
+//...
+
+console.log('Init process');
+hello('Daniel', function (name) {
+  bye(name, function () {
+    console.log('Finish Process...');
+  });
+});
